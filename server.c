c8a6578b9d188a776cf6a7fe12dcf9ea5a9721dd
juicy <wopjr1234@gmail.com>
2025-08-11 19:26:55 -0400

    Updated server to serve till closed

--- server.c
+++ server.c
@@ -6,11 +6,6 @@
 #include <unistd.h>
 
 int main(){
-    struct socket_in{
-        int sa_family;
-        char data;
-    };
-
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if(socketfd == -1){
         perror("Could not create socket\n");
@@ -22,7 +17,7 @@ int main(){
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8888);
-    
+
     if (bind(socketfd, (struct sockaddr*)&address, sizeof(address)) < 0){
         perror("whoops couldn't bind\n");
         return -1;
@@ -32,20 +27,24 @@ int main(){
         perror("Could not listen\n");
         return -1;
     }
-    
-    printf("Now Accepting new connections\n");
 
-    struct sockaddr_in client_address;
-    socklen_t client_len = sizeof(client_address);
-    int new_socket = accept(socketfd, (struct sockaddr *)&client_address, &client_len);
+    while(1){
 
-    if (new_socket < 0){
-        perror("Failed to accept\n");
-        return -1;
+        printf("Now Accepting new connections\n");
+
+        struct sockaddr_in client_address;
+        socklen_t client_len = sizeof(client_address);
+        int new_socket = accept(socketfd, (struct sockaddr *)&client_address, &client_len);
+
+        if (new_socket < 0){
+            perror("Failed to accept\n");
+            return -1;
+        }
+        char buf[19]  = "Hello, World server";
+        send(new_socket, buf, 19, 0);
+
+        close(new_socket);
+        printf("Served one client\n");
     }
-    char buf[19]  = "Hello, World server";
-    send(new_socket, buf, 19, 0);
-
-    close(new_socket);
     close(socketfd);
 }
