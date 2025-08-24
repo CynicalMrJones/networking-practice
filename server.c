eeaf683f3b0ae586abcf199877e01888fb62ee49
juicy <wopjr1234@gmail.com>
2025-08-24 09:47:25 -0400

    Changed server to support new lib folder

--- server.c
+++ server.c
@@ -5,28 +5,12 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <math.h>
 #include <sys/statvfs.h>
-
+#include "lib/stats.h"
 /*
  * Potential Feat: Writing output to a file for logging (Research how logging is typically done)
  */
 
-char *get_stats(){
-    struct statvfs Stats;
-
-    if(statvfs("/", &Stats) == -1){
-        printf("Failed to get stats\n");
-    }
-
-    char *message = (char *)malloc(sizeof(char) * 150);
-    long gb = pow(10, 9);
-    long gib = pow(1024, 3);
-    long total_GB = (Stats.f_bavail * Stats.f_frsize) / gb;
-    long total_GiB = (Stats.f_bavail * Stats.f_frsize) / gib;
-    sprintf(message, "Avaliable Free Blocks: %ld\nTotal avaliable: %ld(GB)\nTotal avaliable: %ld(GiB)\n", Stats.f_bavail, total_GB , total_GiB);
-    return message;
-}
 
 int main(){
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
