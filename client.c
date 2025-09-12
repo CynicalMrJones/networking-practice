e5a13394271d0d1fe40f0a6445663310d8182f4d
juicy <wopjr1234@gmail.com>
2025-09-12 12:48:09 -0400

    Making the buffer bigger Need to find a better way to make this work

--- client.c
+++ client.c
@@ -34,7 +34,7 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    char buf[1000];
+    char buf[10000];
     send(sock, argv[2], sizeof(char[10]), 0);
     int recieve = recv(sock, buf, sizeof(buf) - 1, 0);
     if (recieve < 0){
