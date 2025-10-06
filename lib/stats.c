22b004e7670e74a89fa8bb6cd062e3fbb1e7d956
CynicalMrJones <wopjr1234@gmail.com>
2025-10-06 14:39:18 -0400

    Removed comment

--- lib/stats.c
+++ lib/stats.c
@@ -37,10 +37,6 @@ char *get_temp(){
     return message;
 }
 
-/*
- * BUG: Sometimes get_files does not return the whole list. Look at how 
- * the realloc is behaving. 
- */
 char *get_files(char *path){
     int bufsize = 300;
     int used = 0;
